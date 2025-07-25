1495d42e797fc8195c1f7476cffc83e119ff7e3b
millerr <s0950311@gmailcom>
2025-07-25 08:50:09 +0800

    Bmi專案－初始版本(initial commit)。

---
+++ app/src/main/java/com/millerr/bmi_2025/Hello.kt
@@ -0,0 +1,15 @@
+package com.millerr.bmi_2025
+
+class Hello {
+}
+
+fun main() {
+    println("Hello Kotlin!")
+    val age = 20
+    println(age.inc())
+    println(age)
+    var name = "millerr"
+    println(name.get(1))
+    name = "Jack"
+    println(name)
+}
