1281059db491e2280ab57af4be621729396e6d63
millerr <s0950311@gmailcom>
2025-07-25 14:26:12 +0800

    Hello World練習版本，物件導向初探 Person類別

--- app/src/main/java/com/millerr/bmi_2025/Hello.kt
+++ app/src/main/java/com/millerr/bmi_2025/Hello.kt
@@ -4,8 +4,14 @@ class Hello {
 }
 
 fun main() {
+    val p = Person()
+    p.hello()
+
     println("Hello Kotlin!")
-    val age = 20
+    //Byte, Short, Int, Long
+    val age: Int = 20
+    val population: Long = 9999
+    val population2 = 9999L
     println(age.inc())
     println(age)
     var name = "millerr"
@@ -13,7 +19,12 @@ fun main() {
     name = "Jack"
     println(name)
 
+    //Float(32), Double(64)
+    //有小數點的數字，系統預設會給Double，有三種設定Float的方法
     val weight: Float = 65.5f
+    val weight1 = 65.5f
+    val weight2: Float = 65.5.toFloat()
+
     //true, false
     val isAudit = false
     //Char
---
+++ app/src/main/java/com/millerr/bmi_2025/Person.kt
@@ -0,0 +1,7 @@
+package com.millerr.bmi_2025
+
+class Person {
+    fun hello(){
+        println("Hello")
+    }
+}
